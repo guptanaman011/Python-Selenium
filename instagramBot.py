039c05975696663a4bbdc723cd089e448e0ccfbf
Naman Gupta <62492362+guptanaman011@users.noreply.github.com>
2021-07-09 20:44:13 +0530

    Update instagramBot.py

--- instagramBot.py
+++ instagramBot.py
@@ -6,10 +6,10 @@ browser = webdriver.Chrome(executable_path="./drivers/chromedriver.exe")
 browser.get("https://www.instagram.com")
 sleep(2)
 browser.find_element_by_xpath(
-    "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input").send_keys("cloud1234011")
+    "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input").send_keys("your_username")
 
 browser.find_element_by_xpath(
-    "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input").send_keys("Cloud@123")
+    "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input").send_keys("your_password")
 
 browser.find_element_by_css_selector("#loginForm > div > div:nth-child(3) > button > div").click()
 sleep(5)
